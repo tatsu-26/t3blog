fde921bd6affa5371df7bc832d9a5acbea9d2df7
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-11-27 00:24:27 +0900

    stylesheets作成

--- app/javascript/packs/hoge.js
+++ app/javascript/packs/hoge.js
@@ -2,6 +2,7 @@ import Vue from 'vue/dist/vue.esm.js'
 import Router from "./router/router"
 import VModal from 'vue-js-modal'
 import marked from 'marked';
+import 'stylesheets/application';
 Vue.use(VModal)
 
 Vue.filter("mark-filter", function(showPost){ 
