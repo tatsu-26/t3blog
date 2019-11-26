3d72ea29133c89a106185be60e90425db9c5acc7
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-11-27 02:13:38 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry : './src/entry.js',
+    output : {
+        filename    : 'bundle.js',
+        path        : path.resolve(__dirname, 'dist')
+    }
+};
