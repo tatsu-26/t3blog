e951d615aaff03d05076c878fe4cb465ff852ab3
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-11-28 03:10:12 +0900

    css-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -11,4 +11,12 @@ environment.config.merge(customConfig)
 
 module.exports = environment
 
+const sassLoader = environment.loaders.get('sass')
+const cssLoader = sassLoader.use.find(loader => loader.loader === 'css-loader')
 
+cssLoader.options = Object.assign(cssLoader.options, {
+  modules: true,
+  localIdentName: '[path][name]__[local]--[hash:base64:5]'
+})
+
+module.exports = environment
