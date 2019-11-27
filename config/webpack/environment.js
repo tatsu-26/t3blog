ff5ec0943ea3a6d4be5bc875bf5c6c633dc369ae
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-11-27 17:18:33 +0900

    vue-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
+// const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
