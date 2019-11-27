8f509c9831669f514a294585ca90c768e7a39959
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-11-28 02:44:41 +0900

    test css

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -2,5 +2,7 @@ module.exports = {
   test: /\.vue(\.erb)?$/,
   use: [{
     
-  }]
+  }],
+  test: /\.css$/,
+  loader: "style-loader!css-loader?modules&importLoaders=1&camelCase!postcss-loader"
 }
