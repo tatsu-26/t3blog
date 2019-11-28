82c15b81ae07e73b9fb1d0242787d2266e64437d
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-11-28 15:25:42 +0900

    loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,13 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+//追記
+const tsloaderConf = { test: /\.(ts)?(\.erb)?$/,  loader: 'ts-loader' }
+const tsxloaderConf = { test: /\.(tsx)?(\.erb)?$/,  loader: 'babel-loader!ts-loader' }
+environment.loaders.set('typescript',  tsloaderConf)
+environment.loaders.set('tsx',  tsxloaderConf)
+
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
@@ -11,16 +18,3 @@ environment.config.merge(customConfig)
 
 module.exports = environment
 
-const merge = require('webpack-merge')
-
-const myCssLoaderOptions = {
-  modules: true,
-  sourceMap: true,
-  localIdentName: '[name]__[local]___[hash:base64:5]'
-}
-
-const CSSLoader = environment.loaders.get('style').use.find(el => el.loader === 'css-loader')
-
-CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
-
-module.exports = environment
