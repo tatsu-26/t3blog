61661c074fc5561e7bb32cc4204ebd28a45e84a1
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-11-28 17:12:42 +0900

    module

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,21 +2,17 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+
 //追記
-
-const merge = require('webpack-merge')
-
-const myCssLoaderOptions = {
-  modules: true,
-  sourceMap: true,
-  localIdentName: '[name]__[local]___[hash:base64:5]'
+module.exports = {
+  test: /\.vue(\.erb)?$/,
+  use: [{
+    loader: 'vue-loader',
+    options: { VueLoaderPlugin } 
+  }]
 }
-
-const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
-
-CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
-
 module.exports = environment
+
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
