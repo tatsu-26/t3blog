f787f2edf8f4617fcfa2f96421bd61f01cb10032
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-12-17 17:37:36 +0900

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.177.37.64', user: 'ec2-user', roles: %w{app db web}
+server '3.113.111.97', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
